e606325b8dd942976970eed7bbe38c8c02e6d25d
randomlysa <su@randomlysa.com>
2016-07-20 21:49:30 -0500

    UpdatePost forces strong consistency on post update

--- blog.py
+++ blog.py
@@ -495,6 +495,15 @@ class ToggleLikePost(Handler):
             self.render('editposterror.html')
 
 
+class UpdatePost(Handler):
+    '''Redirect a permalink here to force strong consistency before being
+    redirected back to /blog. Currently used for ToggleLike. '''
+    def get(self, permalink):
+        postOwnerKey = BlogPost.query(BlogPost.permalink == permalink).get().key.parent()
+        postToUpdate = BlogPost.query(ancestor=postOwnerKey).filter(BlogPost.permalink == permalink).get()
+        self.redirect('/blog')
+
+
 app = webapp2.WSGIApplication([
     ('/', RedirectToMainPage),
     ('/blog', MainPage),
@@ -507,5 +516,6 @@ app = webapp2.WSGIApplication([
     ('/blog/([\w\d-]+)/edit', EditPost),
     ('/blog/([\w\d-]+)/delete', DeletePost),
     ('/blog/([\w\d-]+)/like', ToggleLikePost),
+    ('/blog/([\w\d-]+)/update', UpdatePost),
 
 ], debug=True)
