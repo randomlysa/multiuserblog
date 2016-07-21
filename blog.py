facd455991e03b23fc0f29469af58aca5faddea1
randomlysa <su@randomlysa.com>
2016-07-20 21:49:52 -0500

    use UpdatePost on toggle like

--- blog.py
+++ blog.py
@@ -481,14 +481,14 @@ class ToggleLikePost(Handler):
             if self.get_userid() in postToToggleLike.likes:
                 postToToggleLike.likes.remove(str(self.get_userid()))
                 postToToggleLike.put()
-                self.redirect('/blog')
+                self.redirect('/blog/%s/update' % permalink)
 
             # if the user has NOT liked the post, like it
             else:
                 # append userid to the db
                 postToToggleLike.likes.append(str(self.get_userid()))
                 postToToggleLike.put()
-                self.redirect('/blog')
+                self.redirect('/blog/%s/update' % permalink)
         # post not found or user is owner
         else:
             # make some kind of error handler later
